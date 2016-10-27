25d91b0da660544a7791be20dad7eba52bbcb59e
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2016-10-27 15:49:08 -0700

    ENH added json reading utilies
    - Added mockdata for matplotlib/matplotlib for commits and issues.
    - Added utility functions to read the json files kept in the data folder.

--- watchtower/tests/test_config.py
+++ watchtower/tests/test_config.py
@@ -1,8 +1,12 @@
+import os
+from os.path import dirname
 import tempfile
+
 from watchtower._config import get_data_home, clear_data_home
+from watchtower._config import get_issues, get_commits, get_PRs
+
 from watchtower.utils.testing import assert_true, assert_false
 from watchtower.utils.testing import assert_equal
-import os
 
 
 DATA_HOME = tempfile.mkdtemp(prefix="watchtower_data_home_test_")
@@ -21,3 +25,36 @@ def test_data_home():
     # if the folder is missing it will be created again
     data_home = get_data_home(data_home=DATA_HOME)
     assert_true(os.path.exists(data_home))
+
+
+def test_get_issues():
+    issues = get_issues("matplotlib", "matplotlib", data_home=DATA_HOME)
+    clear_data_home(data_home=DATA_HOME)
+    assert_equal(issues, None)
+
+    # Now use some mockdata
+    data_home = os.path.join(dirname(__file__), "../mockdata")
+    issues = get_issues("matplotlib", "matplotlib", data_home=data_home)
+    assert_true(len(issues))
+
+
+def test_get_commits():
+    commits = get_commits("matplotlib", "matplotlib", data_home=DATA_HOME)
+    clear_data_home(data_home=DATA_HOME)
+    assert_equal(commits, None)
+
+    # Now use some mockdata
+    data_home = os.path.join(dirname(__file__), "../mockdata")
+    commits = get_commits("matplotlib", "matplotlib", data_home=data_home)
+    assert_true(len(commits))
+
+
+def test_get_PRs():
+    PRs = get_PRs("matplotlib", "matplotlib", data_home=DATA_HOME)
+    clear_data_home(data_home=DATA_HOME)
+    assert_equal(PRs, None)
+
+    # Now use some mockdata
+    data_home = os.path.join(dirname(__file__), "../mockdata")
+    PRs = get_PRs("matplotlib", "matplotlib", data_home=data_home)
+    assert_true(len(PRs))
