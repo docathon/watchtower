2b1a9de6a5278fd36d1f10379debfb54ef27cbf9
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-02-02 15:21:58 -0800

    DOC added some examples

---
+++ examples/plot_doc_commits.py
@@ -0,0 +1,44 @@
+from watchtower import commits_
+import numpy as np
+import matplotlib.pyplot as plt
+
+projects = ["matplotlib", "numpy", "scikit-learn"]
+for project in projects:
+    user = project
+    commits = commits_.get_commits(user, project)
+
+    n_commits = commits.shape[0]
+    doc_commits = sum("DOC" in commits.commit[i]["message"]
+                    for i in range(n_commits))
+    n_commits = []
+    doc_commits = []
+    for month in range(1, 13):
+        dtime = "2016-%02d" % month
+        mask = commits.commit.apply(
+            lambda x: x["author"]["date"].startswith(dtime))
+        n_commits.append(mask.sum())
+        doc_commits.append(sum(
+            ("DOC" in c["message"]) | ("docs" in c["message"]) | ("docstring" in c["message"])
+             for c in commits[mask].commit))
+
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.bar(np.arange(len(n_commits)), n_commits, label="all")
+    ax.bar(np.arange(len(n_commits)), doc_commits, label="doc")
+    ax.grid("off")
+    ax.spines['right'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    label_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+                "Oct", "Nov", "Dec"]
+    ax.set_xticks(np.arange(12))
+    ax.set_xticklabels(label_names, rotation=90, fontsize="x-small")
+    ax.set_ylabel("# commits")
+
+    yticks = ax.get_yticks()
+    for l in yticks:
+        ax.axhline(l, linewidth=0.75, zorder=-10, color="0.5")
+    ax.set_yticks(yticks)
+    ax.legend(loc=1)
+    ax.set_title(project, fontweight="bold")
---
+++ examples/plot_labels.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from watchtower import issues_
+from watchtower import labels_
+import matplotlib.pyplot as plt
+
+projects = ("matplotlib", "scikit-learn", "scikit-image", "ipython", "numpy")
+
+for project in projects:
+    user = project
+
+    all_issues = issues_.get_issues(user, project, issues_type="all")
+    opened_issues = issues_.select_opened(all_issues)
+
+    # Get all labels
+    labels = labels_.get_labels(user, project)
+    # Extract the names of the labels
+    labels_names = sorted(labels.name.as_matrix())
+    labels_counts = []
+    opened_labels = []
+
+    for name in labels_names:
+        labels_counts.append(sum(
+            [1 for label in all_issues["labels"]
+             for l in label if l["name"] == name]))
+        opened_labels.append(sum(
+            [1 for label in opened_issues["labels"]
+             for l in label if l["name"] == name]))
+
+    fig = plt.figure(figsize=(14, 5))
+    ax = fig.add_axes([0.1, 0.3, 0.8, 0.6])
+    ax.grid("off")
+    ax.spines['right'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    ax.bar(np.arange(len(labels_names)), labels_counts, label="Closed")
+    ax.bar(np.arange(len(labels_names)), opened_labels, label="Open")
+
+    ax.set_xticks(np.arange(len(labels_names)))
+    ax.set_xticklabels(labels_names, rotation=90, fontsize="x-small")
+
+    yticks = ax.get_yticks()
+    for l in yticks:
+        ax.axhline(l, linewidth=0.75, zorder=-10, color="0.5")
+    ax.set_yticks(yticks)
+    ax.set_title(project, fontweight="bold")
+
+    ax.legend()
