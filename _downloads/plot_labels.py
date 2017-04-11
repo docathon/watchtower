b17847da8780ef76d71448e728f2900afcf3c152
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2017-04-11 01:45:58 +0000

    Update docs after building Travis build 31 of docathon/watchtower
    The docs were built from the branch 'master' against the commit
    479a58754948a4ac259c903ad2ded0d3150b4456.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/docathon/watchtower/jobs/220789299.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.0/bin/doctr deploy .

---
+++ _downloads/plot_labels.py
@@ -0,0 +1,72 @@
+"""
+======
+Labels
+======
+
+In this small examples, we compare labels between the different projects:
+
+    - scikit-learn
+    - watchtower
+
+"""
+import numpy as np
+import pandas as pd
+from watchtower import GithubDatabase
+import matplotlib.pyplot as plt
+
+projects = (("scikit-learn", "scikit-learn"),
+            ("docathon", "watchtower"))
+db = GithubDatabase(verbose=True)
+
+update_issues = True  # Do we update the database
+since = '2017-02-01'
+
+for user, project in projects:
+    if update_issues is True:
+        db.update(user, project, since=since)
+
+    proj = db.load(user, project)
+    if proj.issues is None:
+        print('No data for {}'.format(project))
+        continue
+    all_issues = proj.issues
+    open_issues = all_issues.query('state == "open"')
+
+    # Extract the names of the labels
+    all_labels = np.array([name for names in all_issues['label_names'].values
+                           for name in names])
+    open_labels = np.array([name for names in open_issues['label_names'].values
+                            for name in names])
+    unique_labels = np.unique(all_labels)
+    counts = dict()
+    for label in unique_labels:
+        n_instances = np.sum(all_labels == label)
+        n_open = np.sum(open_labels == label)
+        counts[label] = (n_instances, n_open)
+    counts = pd.DataFrame(counts).T
+    counts.columns = ['all', 'open']
+    counts = counts.sort_values('all', ascending=False)
+
+    fig = plt.figure(figsize=(14, 5))
+    ax = fig.add_axes([0.1, 0.3, 0.8, 0.6])
+    ax.grid("off")
+    ax.spines['right'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    for column in counts.columns:
+        ixs = range(counts.shape[0])
+        ax.bar(ixs, counts[column], label=column)
+    ax.set_xticks(ixs)
+    ax.set_xticklabels(counts.index, rotation=90, fontsize="x-small")
+
+    yticks = ax.get_yticks()
+    for l in yticks:
+        ax.axhline(l, linewidth=0.75, zorder=-10, color="0.5")
+    ax.set_yticks(yticks)
+    ax.set_title(project, fontweight="bold")
+
+    ax.legend()
+plt.show()
---
+++ _downloads/plot_project_data.py
@@ -0,0 +1,57 @@
+"""
+=================================
+Identifying documentation commits
+=================================
+
+This example shows how to identify documentation specfic commits.
+
+"""
+from watchtower.handlers_ import GithubDatabase
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+projects = [('docathon', 'watchtower'),
+            ('scikit-learn', 'scikit-learn')]
+
+
+def is_doc(string):
+    check_strings = ['doc', 'documentation', 'docathon']
+    is_doc = any(istr in string for istr in check_strings)
+    return is_doc
+
+
+update_db = True
+
+# Initialize the database
+db = GithubDatabase(verbose=True)
+
+# Update users and print the db
+since = '2017-02-10'
+if update_db is True:
+    for user, project in projects:
+        db.update(user, project, since=since)
+since = pd.to_datetime(since)
+print(db)
+
+# Load the data for plotting
+projects = [db.load(user, project) for user, project in projects]
+
+# Now plot pushes each day
+fig, axs = plt.subplots(nrows=2, figsize=(8, 6), sharex=True, sharey=True)
+for proj, ax in zip(projects, axs):
+    proj.commits['is_doc'] = proj.commits['message'].apply(is_doc)
+    proj.commits = proj.commits.query('date > @since')
+    all_commits = proj.commits.resample('D').count().\
+        replace(np.nan, 0).astype(int)
+    doc_commits = proj.commits.resample('D').sum().\
+        replace(np.nan, 0).astype(int)
+    ax.bar(all_commits.index, all_commits['is_doc'], label='all')
+    ax.bar(doc_commits.index, doc_commits['is_doc'], label='doc')
+    ax.set_title("{}/{}".format(proj.user, proj.project), fontweight="bold")
+    ax.set_ylabel('# Commits', fontweight="bold")
+axs[-1].legend()
+plt.setp(axs[-1].get_xticklabels(), rotation=45, horizontalalignment='right')
+plt.setp([ax.xaxis.label for ax in axs], visible=False)
+plt.tight_layout()
+plt.show()
