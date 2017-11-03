5f0d37b8391c1d954f80abbf74b9d92eb161c53f
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-11-03 11:01:13 -0700

    More cleaning up.

--- watchtower/issues_.py
+++ watchtower/issues_.py
@@ -2,7 +2,6 @@ import os
 from os.path import join
 
 import pandas as pd
-import numpy as np
 
 from . import _github_api
 from ._config import get_data_home, get_API_token
@@ -103,71 +102,3 @@ def load_issues(user, project, data_home=None,
     except ValueError:
         return None
     return issues
-
-
-class ProjectIssues(object):
-    """Represent issue for a github project.
-
-    This stores information about issue activity over time.
-
-    Parameters
-    ----------
-    user : string
-        The user to query
-
-    project : string
-        The repository to query.
-
-    raw : json return by github api
-        The raw data for this project, as returned by the github api.
-
-    Attributes
-    ----------
-    issues : DataFrame
-        A collection of issues activity for this project.
-    label_names : list of strings
-        A list of strings representing each label in the project
-    """
-    def __init__(self, user, project, raw):
-        self.user = user
-        self.project = project
-        raw = raw.set_index('date')
-        raw.index = pd.to_datetime(raw.index)
-
-        self.raw = raw
-
-        # Extract issue information that is useful
-        # Building a dictionary in case we need to dig
-        # deeper in future versions of watchtower
-        issues = dict(
-            title=[], labels=[], state=[],
-            updated_at=[], html_url=[], id=[],
-            user=[], pull_request=[])
-        if len(raw) == 0:
-            self.issues = None
-            return
-        use_keys = list([key for key in issues.keys()
-                         if key in raw.columns])
-        insert_keys = list([key for key in issues.keys()
-                            if key not in raw.columns])
-        issues = raw[use_keys]
-
-        # Label names
-        label_names = [list(label['name']
-                            for label in labels)
-                       for labels in issues['labels'].values]
-        issues['label_names'] = label_names
-        for key in insert_keys:
-            issues[key] = np.nan
-
-        # Has a PR
-        has_pr = -1 * issues['pull_request'].isnull()
-        issues['has_pr'] = has_pr
-        self.issues = issues
-
-    def __repr__(self):
-        s = 'Issues: {} / {} | N Open: {} | N Closed: {}'.format(
-            self.user, self.project,
-            sum(self.issues['state'] == 'open'),
-            sum(self.issues['state'] == 'closed'))
-        return s
